3d373f7b5695985b1ed560c28321bac07ed29c15
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2023-01-06 20:21:37 +0200

    Show Results in csv file

--- main.cpp
+++ main.cpp
@@ -3,105 +3,11 @@
 #include <chrono>
 
 using namespace std;
+typedef __int128 int128_t;
+typedef unsigned __int128 uint128_t;
 
-
-// initialize A as lower triangular matrix with ones
-// initialize B as upper triangular matrix with ones
-template <typename value_t,typename index_t>
-void init(std::vector<value_t>& A,
-          std::vector<value_t>& B,
-          index_t n) {
-
-    for (index_t row = 0; row < n; row++)
-        for (index_t col = 0; col < n; col++)
-            A[row*n+col] = row >= col ? 1 : 0;
-
-    for (index_t row = 0; row < n; row++)
-        for (index_t col = 0; col < n; col++)
-            B[row*n+col] = row <= col ? 1 : 0;
-}
-
-// the sequential sum of 2 matrices
 template <typename value_t, typename index_t>
-void sequential_add(
-        std::vector<value_t>& A, std::vector<value_t>& B, std::vector<value_t>& C, index_t n) {
-
-    for (index_t row = 0; row < n; row++) {
-        for (index_t col = 0; col < n; col++)
-            C[row*n + col] = A[row*n + col] + B[row*n + col];
-    }
-}
-
-
-
-// Matrix sum template using a static BLOCK distribution template
-template <typename value_t, typename index_t>
-void block_parallel_sum (std::vector<value_t>& A, std::vector<value_t>& B,
-                         std::vector <value_t>& C, index_t n, index_t num_threads = 8) {
-    auto block = [&] (const index_t& id) -> void {
-        for (index_t row = id; row < n; row+=num_threads) {
-            for (index_t col = 0; col < n; col++) {
-                C[row*n + col] = A[row*n + col] + B[row*n + col];
-            }
-        }
-    };
-    std::vector<std::thread> threads;
-    for (index_t id = 0; id < num_threads; id++) threads.emplace_back(block, id);
-    for (auto& thread : threads) thread.join();
-}
-
-
-// Matrix sum template using a static BLOCK_CYCLIC distribution template
-template <typename value_t, typename index_t>
-void block_cyclic_parallel_sum(std::vector<value_t>& A, std::vector<value_t>& B,
-                        std::vector <value_t>& C, index_t n, index_t num_threads = 8,
-                        index_t chunk_size=64/sizeof(value_t)) {
-    auto block = [&](const index_t& id) -> void {
-        const index_t offset = id*chunk_size;
-        const index_t stride = num_threads*chunk_size;
-        for (index_t lower = offset; lower < n; lower += stride){
-            const index_t upper = std::min(lower + chunk_size, n);
-            for (index_t row = lower; row < upper; row++) {
-                for (index_t col = 0; col < n; col++)
-                    C[row*n + col] = A[row*n + col] + B[row*n + col];
-            }
-        }
-    };
-    std::vector<thread> threads;
-    for (index_t id = 0; id < num_threads; id++) threads.emplace_back(block, id);
-    for (auto& thread : threads) thread.join();
-}
-
-// Matrix sum template using a static CYCLIC distribution template
-template <typename value_t, typename index_t>
-void cyclic_parallel_sum(std::vector<value_t>& A, std::vector<value_t>& B,
-                        std::vector <value_t>& C, index_t n, index_t num_threads = 8) {
-    auto block = [&](const index_t& id) -> void {
-        for (index_t row = id; row < n; row+=num_threads){
-            for (index_t col = 0; col < n; col++)
-                C[row*n + col] = A[row*n + col] + B[row*n + col];
-        }
-    };
-    std::vector<thread> threads;
-    for (index_t id = 0; id < num_threads; id++)
-        threads.emplace_back(block, id);
-    for (auto& thread : threads) thread.join();
-}
-
-int main(int argc, char* argv[]) {
-    const uint64_t n = 16;
-
-    std::vector<uint64_t> A(n*n);   // alloc
-    std::vector<uint64_t> B(n*n);
-    std::vector<uint64_t> C(n*n);
-
-    init(A, B, n);               // init
-    auto start = std::chrono::high_resolution_clock::now();
-    cyclic_parallel_sum(A, B, C, n); // add
-    auto stop = std::chrono::high_resolution_clock::now();
-
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    cout<<duration.count()<<endl;
+void printInitials(vector<value_t>& A, vector<value_t>& B,index_t n){
     cout<<"A:"<<endl;
     for (uint64_t row = 0; row < n; row++){
         for (uint64_t col = 0; col < n; col++)
@@ -115,13 +21,141 @@ int main(int argc, char* argv[]) {
             cout<<B[row*n+col]<<" ";
         cout << endl;
     }
+}
 
-    cout<<"C:"<<endl;
+// initialize A as lower triangular matrix with ones
+// initialize B as upper triangular matrix with ones
+template <typename value_t,typename index_t>
+void init(vector<value_t>& A,vector<value_t>& B,index_t n) {
+    for (index_t row = 0; row < n; row++)
+        for (index_t col = 0; col < n; col++)
+            A[row*n+col] = row >= col ? 1 : 0;
 
+    for (index_t row = 0; row < n; row++)
+        for (index_t col = 0; col < n; col++)
+            B[row*n+col] = row <= col ? 1 : 0;
+
+//    printInitials(A,B,n);
+}
+
+// the sequential sum of 2 matrices
+template <typename value_t, typename index_t>
+long long int sequential_add(vector<value_t>& A, vector<value_t>& B, vector<value_t>& C, index_t n) {
+    auto start = chrono::high_resolution_clock::now();
+    for (index_t row = 0; row < n; row++) {
+        for (index_t col = 0; col < n; col++)
+            C[row*n + col] = A[row*n + col] + B[row*n + col];
+    }
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    return duration.count();
+}
+
+
+
+// Matrix sum template using a static BLOCK distribution template
+template <typename value_t, typename index_t>
+long long int block_parallel_sum (vector<value_t>& A, vector<value_t>& B,vector <value_t>& C, index_t n, index_t num_threads = 8) {
+    auto start = chrono::high_resolution_clock::now();
+    auto block = [&](const index_t& id) -> void {
+        const index_t chunk = ceil((double)n/num_threads);
+        const index_t lower = id*chunk;
+        const index_t upper = std::min(lower + chunk, n);
+        for (index_t row = lower; row < upper; row++) {
+            for (index_t col = 0; col < n; col++)
+                C[row*n + col] = A[row*n + col] + B[row*n + col];
+        }
+    };
+    vector<thread> threads;
+    for (index_t id = 0; id < num_threads; id++) threads.emplace_back(block, id);
+    for (auto& thread : threads) thread.join();
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    return duration.count();
+}
+
+
+// Matrix sum template using a static BLOCK_CYCLIC distribution template
+template <typename value_t, typename index_t>
+long long int block_cyclic_parallel_sum(vector<value_t>& A, vector<value_t>& B,vector <value_t>& C, index_t n, index_t num_threads = 8,
+                        index_t chunk_size=64/sizeof(value_t)) {
+    auto start = chrono::high_resolution_clock::now();
+    auto block = [&](const index_t& id) -> void {
+        const index_t offset = id*chunk_size;
+        const index_t stride = num_threads*chunk_size;
+        for (index_t lower = offset; lower < n; lower += stride){
+            const index_t upper = min(lower + chunk_size, n);
+            for (index_t row = lower; row < upper; row++) {
+                for (index_t col = 0; col < n; col++)
+                    C[row*n + col] = A[row*n + col] + B[row*n + col];
+            }
+        }
+    };
+    vector<thread> threads;
+    for (index_t id = 0; id < num_threads; id++) threads.emplace_back(block, id);
+    for (auto& thread : threads) thread.join();
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    return duration.count();
+}
+
+// Matrix sum template using a static CYCLIC distribution template
+template <typename value_t, typename index_t>
+long long int cyclic_parallel_sum(vector<value_t>& A, vector<value_t>& B,vector <value_t>& C, index_t n, index_t num_threads = 8) {
+    auto start = chrono::high_resolution_clock::now();
+    auto block = [&](const index_t& id) -> void {
+        for (index_t row = id; row < n; row+=num_threads){
+            for (index_t col = 0; col < n; col++)
+                C[row*n + col] = A[row*n + col] + B[row*n + col];
+        }
+    };
+    vector<thread> threads;
+    for (index_t id = 0; id < num_threads; id++)
+        threads.emplace_back(block, id);
+    for (auto& thread : threads) thread.join();
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
+    return duration.count();
+}
+
+
+template <typename value_t, typename index_t>
+void printResults(vector <value_t>& C,index_t n, const string& title){
+    cout<< title<<":"<<endl;
     for (uint64_t row = 0; row < n; row++){
         for (uint64_t col = 0; col < n; col++)
             cout<<C[row*n+col]<<" ";
         cout << endl;
     }
+}
 
+int main() {
+    std::ofstream results;
+    results.open ("results.csv");
+    results << ",,block_parallel_sum,,,,,,,block_cyclic_parallel_sum,,,,,,,cyclic_parallel_sum\n";
+    results << "Matrix Size,sequential add" << endl;
+    uint64_t size = 1;
+    for (int i = 1 ; i < 15 ; i++){
+        size <<= 1;
+//        cout << size << endl;
+        vector<uint64_t> A(size*size);   // alloc
+        vector<uint64_t> B(size*size);
+        vector<uint64_t> C(size*size);
+        init(A, B, size); // init
+        results << size << "," << sequential_add(A,B,C,size);
+        for (uint64_t threads = 8 ; threads < 513; threads*=2){
+//            cout << threads << endl;
+            results <<","<<block_parallel_sum(A,B,C,size,threads);
+        }
+        for (uint64_t threads = 8 ; threads < 513; threads*=2){
+            results <<","<<block_cyclic_parallel_sum(A,B,C,size,threads);
+        }
+        for (uint64_t threads = 8 ; threads < 513; threads*=2){
+            results <<","<<cyclic_parallel_sum(A,B,C,size,threads);
+        }
+        results << endl;
+    }
+    //    printResults(C,n,"");
+    results.close();
+    return 0;
 }
